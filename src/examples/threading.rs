ced0473c5d518040e9ffa46f94da30f547a200e4
Lovro Markovic <lovro.markovic@fer.hr>
2023-11-04 09:38:14 +0100

    Add some more threading

--- src/examples/threading.rs
+++ src/examples/threading.rs
@@ -4,9 +4,11 @@ use examples::threading::rayon::iter::IntoParallelRefMutIterator;
 use examples::threading::rayon::iter::ParallelIterator;
 
 use std::sync::atomic::AtomicI32;
+use std::sync::Arc;
 use std::sync::Mutex;
 use std::sync::MutexGuard;
 use std::sync::RwLock;
+use std::thread;
 
 /*
  * Magical multithreading aliasing examples.
@@ -135,4 +137,59 @@ pub fn sync_shared_state_atomic() {
     });
 
     assert_eq!(*atomic_sum.get_mut(), 15);
+}
+
+/*
+ * Arc mutex string example.
+ */
+#[allow(unused_must_use)]
+pub fn freestyle_mutltithreading() {
+    // let my_string: Mutex<String> = Mutex::new("abcd".to_string());
+    let arc_mutex_string: Arc<Mutex<String>> = Arc::new(Mutex::new(String::new()));
+
+    let mut thread_handles = Vec::new();
+
+    for _ in 1..10 {
+        // Need to first make a local clone of the shared_ptr in order to move it to 
+        // the thread. Moving arc_mutex_string to a thread would work for only 
+        // one thread. 
+        // The ownership of the arc_clone is transferred to the thread which 
+        // mentions its name.
+        let arc_clone = arc_mutex_string.clone();
+
+        let thread_handle = thread::spawn(move || {
+            // let mut mut_guard = my_string.lock().unwrap();
+            // mut_guard.push_str("some_characters");
+
+            // Do the Arc!
+            let mut guard = arc_clone.lock().unwrap();
+            guard.push_str("some_chars");
+        });
+
+        thread_handles.push(thread_handle);
+    }
+
+    // Rust is worried about the following.
+    // If the outer function somhow returns before joining threads (e.g. spawning
+    // of a thread could totaly throw an exeption, in c++ if there is an exeption
+    // in the thread,c++ calls std::terminate killing the entire program, depending on
+    // the order of initialization if threads are declared after resources they
+    // use then it's fine, but if they are declared before then it may be a
+    // use-after-free before threads can actually terminate) then
+    // my_string would be destroyed and its reference in the closure would
+    // cause use-after-free which is a compiler error.
+    //
+    // WE know that join is happening and everything is fine. Rust doesn't.
+    //
+    // To circumvent this problem it's beneficial to use std::shared_ptr<..> in C++ and
+    // capture it by VALUE in lambda [=], or use it's equivalent in Rust - Arc.
+    // Similar to Arc, the Box is unique_ptr equivalent.
+
+    for thread_handle in thread_handles {
+        thread_handle.join().unwrap();
+    }
+
+    {
+        dbg!(arc_mutex_string.lock().unwrap());
+    }
 }
