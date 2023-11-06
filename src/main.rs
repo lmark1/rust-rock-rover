bc852f318b25a5a8a261889b9910db58f5f92949
Lovro Markovic <lovro.markovic@fer.hr>
2023-11-06 17:57:30 +0100

    Politely crawl

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use spider::website::Website;
 use std::io::Error;
 use std::time::{Duration, Instant};
 
-use http::header::{ACCEPT, ACCEPT_LANGUAGE, CACHE_CONTROL, CONNECTION, USER_AGENT};
+use http::header::{ACCEPT, ACCEPT_LANGUAGE, CACHE_CONTROL, CONNECTION, USER_AGENT, UPGRADE_INSECURE_REQUESTS};
 use http::HeaderMap;
 
 // Tokio main is needed to use async function
@@ -26,17 +26,26 @@ async fn main() -> Result<(), Error> {
     headers.insert(CACHE_CONTROL, "max-age=0".parse().unwrap());
     headers.insert(USER_AGENT, "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36".parse().unwrap());
     headers.insert(CONNECTION, "keep-alive".parse().unwrap());
+    headers.insert(UPGRADE_INSECURE_REQUESTS, "1".parse().unwrap());
 
     website
         .with_headers(Some(headers))
         .with_subdomains(true)
-        .with_delay(0)
+        .with_respect_robots_txt(true)
+        .with_delay(150)
         .with_request_timeout(Some(Duration::new(10, 0)))
         .with_http2_prior_knowledge(false)
         .with_proxies(None);
 
+    website.on_link_find_callback = Some(|s, ss| {
+        println!("link target: {:?}", s);
+        // forward link to a different destination
+        (s,ss)
+    });
+
     let start = Instant::now();
     website.crawl().await;
+
     let duration = start.elapsed();
 
     let links = website.get_links();
